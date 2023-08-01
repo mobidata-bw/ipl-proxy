15748011b9162a9a3559af6fc2e593cfb784d6ab
Holger Bruch <hb@mfdz.de>
2023-08-01 11:19:01 +0200

    GBFS: move transforming logic to utils

--- app/converters/gbfs_nextbike_duplicates.py
+++ app/converters/gbfs_nextbike_duplicates.py
@@ -7,7 +7,7 @@ All rights reserved.
 from typing import List, Union
 
 from app.base_converter import BaseConverter
-
+from app.utils.gbfs import filter_duplicate_vehicle_types
 
 class GbfsNextbikeDuplicatesConverter(BaseConverter):
     hostnames = ['gbfs.nextbike.net']
@@ -16,17 +16,4 @@ class GbfsNextbikeDuplicatesConverter(BaseConverter):
         if not isinstance(data, dict) or not path.startswith('/maps/gbfs/v2/') or not path.endswith('/vehicle_types.json'):
             return data
 
-        if not data.get('data', {}).get('vehicle_types'):
-            return data
-
-        known_ids: List[str] = []
-        filtered_vehicle_types: List[dict] = []
-
-        for vehicle_type in data['data']['vehicle_types']:
-            if vehicle_type.get('vehicle_type_id') in known_ids:
-                continue
-            known_ids.append(vehicle_type.get('vehicle_type_id'))
-            filtered_vehicle_types.append(vehicle_type)
-
-        data['data']['vehicle_types'] = filtered_vehicle_types
-        return data
+        return filter_duplicate_vehicle_types(data)
