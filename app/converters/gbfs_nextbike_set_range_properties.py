e74068dcb43578b07be58d2397b3297a92e63b79
Thorsten Fröhlinghaus <thorsten.froehlinghaus@nvbw.de>
2024-06-28 07:37:06 +0200

    check propulsion_type

--- app/converters/gbfs_nextbike_set_range_properties.py
+++ app/converters/gbfs_nextbike_set_range_properties.py
@@ -12,12 +12,18 @@ class GbfsNextbikeSetRangePropertiesConverter(BaseConverter):
 
         if path.endswith('/vehicle_types.json'):
             for vehicle_type in data['data'].get('vehicle_types', []):
+                if 'propulsion_type' not in vehicle_type or vehicle_type['propulsion_type'] == 'human':
+                    continue
                 if 'max_range_meters' not in vehicle_type or vehicle_type['max_range_meters'] == 0:
                     vehicle_type['max_range_meters'] = 60000
             return data
 
         if path.endswith('/free_bike_status.json'):
             for bike in data['data'].get('bikes', []):
+                if 'vehicle_type_id' not in bike:
+                    continue
+                # vehicle_type_id = bike['vehicle_type_id']
+                # check if vehicle_type has propulsion_type != human
                 if 'current_range_meters' not in bike or bike['current_range_meters'] == 0:
                     bike['current_range_meters'] = 1000
             return data
