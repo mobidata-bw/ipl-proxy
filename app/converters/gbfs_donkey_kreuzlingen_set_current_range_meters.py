df126b2b36bbe4377b191a2721ceab098dd55b99
Thorsten Fröhlinghaus <120580045+ThorstenFroehlinghaus@users.noreply.github.com>
2024-08-22 11:13:26 +0200

    Create gbfs_donkey_kreuzlingen_set_current_range_meters.py

---
+++ app/converters/gbfs_donkey_kreuzlingen_set_current_range_meters.py
@@ -0,0 +1,31 @@
+from typing import List, Union
+
+from app.base_converter import BaseConverter
+
+
+class GbfsDonkeyKreuzlingenSetCurrentRangeMetersConverter(BaseConverter):
+    hostnames = ['stables.donkey.bike']
+
+    @staticmethod
+    def _get_system_id_from_path(path: str) -> str:
+        return path.split('/')[-3]
+
+    def convert(self, data: Union[dict, list], path: str) -> Union[dict, list]:
+        if not isinstance(data, dict):
+            return data
+
+        if path.endswith('/free_bike_status.json'):
+            system_id = self._get_system_id_from_path(path)
+            if system_id != 'donkey_kreuzlingen':
+                return data
+            vehicles = data.get('data', {}).get('bikes', [])
+            if not isinstance(vehicles, list):
+                return data
+            for vehicle in vehicles:
+                if 'current_range_meters' in vehicle and vehicle['current_range_meters'] is None:
+                    vehicle['current_range_meters'] = 1000
+                if 'current_fuel_percent' in vehicle and vehicle['current_fuel_percent'] is None:
+                    del vehicle['current_fuel_percent']
+            return data
+
+        return data
