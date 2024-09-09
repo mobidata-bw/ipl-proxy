770d1fcb2ad695be090869f060ccfb3ec160d9e1
Thorsten Fröhlinghaus <thorsten.froehlinghaus@nvbw.de>
2024-09-09 15:13:49 +0200

    nextbike_set_form_factor

---
+++ app/converters/gbfs_nextbike_set_form_factor.py
@@ -0,0 +1,25 @@
+from typing import List, Union
+
+from app.base_converter import BaseConverter
+
+
+class GbfsNextbikeSetFormFactorConverter(BaseConverter):
+    hostnames = ['gbfs.nextbike.net']
+
+    def convert(self, data: Union[dict, list], path: str) -> Union[dict, list]:
+        if not isinstance(data, dict):
+            return data
+
+        if path.endswith('/vehicle_types.json'):
+            fields = data.get('data', {})
+            if not isinstance(fields, dict):
+                return data
+            vehicle_types = fields.get('vehicle_types', [])
+            if not isinstance(vehicle_types, list):
+                return data
+            for vehicle_type in vehicle_types:
+                if 'form_factor' not in vehicle_type:
+                    vehicle_type['form_factor'] = 'other'
+            return data
+
+        return data
